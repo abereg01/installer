906999dfc992705855b10a66095e6a6a8d8cb70a
Andreas Bergholtz <abereg01@pm.me>
2023-09-19 13:30:22 +0200

    additional edits

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#This script was made by Abereg01, with modifications from various sources.
+# This script was made by Scooby. With borrowed code that has been heavily modified. 
 
 echo 'Scooby Debian/DWM installation script'
 
@@ -29,6 +29,11 @@ sudo systemctl enable ly.service
 #Software
 sudo apt install $(cat $HOME/installer/installation_files/pkglist | perl -pe 's|\n| |g') -y
 
+sudo systemctl enable bluetooth
+sudo systemctl enable cups
+
+#getNF
+
 #Pfetch
 #Hitta install script
 
@@ -37,6 +42,43 @@ sudo apt install $(cat $HOME/installer/installation_files/pkglist | perl -pe 's|
 
 #Starship
 curl -sS https://starship.rs/install.sh | sh
+
+
+# Xsession & DWM
+if [[ ! -d /usr/share/xsessions ]]; then
+	sudo mkdir /usr/share/xsessions
+fi
+
+cat > ./temp << "EOF"
+[Desktop Entry]
+Encoding=UTF-8
+Name=dwm
+Comment=Dynamic window manager
+Exec=dwm
+Icon=dwm
+Type=Xsession
+EOF
+sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
+
+#SUCKLESS
+#cd && mkdir $HOME/.config/suckless && cd $HOME/.config/suckless/
+#tools=( "dwm" "dmenu" "st" "slstatus" "slock" "tabbed" )
+#for tool in ${tools[@]}
+#do
+#	git clone git://git.suckless.org/$tool
+#	cd $HOME/.config/suckless/$tool;make;sudo make clean install;cd ..
+#done
+#
+#cd && rm -rf $HOME/.config/suckless/
+
+# SYMLINK SUCKLESS
+# MAKE CLEAN INSTALL
+
+# SYMLINK
+# SYMLINK DOC
+
+
+
 
 #Installer Removal
 cd && rm -rf $HOME/XXXXX
