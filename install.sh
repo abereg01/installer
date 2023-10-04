908b958ea366c9a54639e1228c77fd98c2a9bfae
Scoob <106392600+abereg01@users.noreply.github.com>
2023-10-04 09:49:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,43 +4,46 @@ echo 'Scooby installation script'
 mkdir $HOME/.config/ $HOME/scripts/ $HOME/downloads
 
 # Base installation
-sudo apt install $(cat $HOME/installer/installation_files/base) -y
-sudo systemctl enable avahi-daemon
+sudo apt install $(cat $HOME/installer/installation_files/base) curl wget -y
+wait
+sudo systemctl enable avahi-daemon &
 sudo systemctl enable acpid
 
 # Sources.list
-sudo chmod +x $HOME/installer/installation_files/sourceslist.sh
-sh $HOME/installer/installation_files/sourceslist.sh
-sudo apt update
+sudo chmod +x $HOME/installer/installation_files/sourceslist.sh &
+sh $HOME/installer/installation_files/sourceslist.sh 
+sudo apt update 
 
 # Ly
-cd && mkdir -p software/ly && cd software/ly/
+cd && mkdir software/ && cd software/
 sudo apt install -y libpam0g-dev libxcb-xkb-dev
 git clone --recurse-submodules https://github.com/fairyglade/ly
+cd ly/
 make
-sudo make install installsystmd
+make run
+sudo make install installsystemd
 sudo systemctl enable ly.service
 
 # Software
-sudo apt install $(cat $HOME/installer/installation_files/pkglist) -y
-sudo systemctl enable bluetooth
-sudo systemctl enable cups
+#sudo apt install $(cat $HOME/installer/installation_files/pkglist) -y
+#sudo systemctl enable bluetooth
+#sudo systemctl enable cups
 
 # Nvim
-sudo chmod +x $HOME/installer/installation_files/nvim.sh
-sh $HOME/installer/installation_files/nvim.sh
+#sudo chmod +x $HOME/installer/installation_files/nvim.sh
+#sh $HOME/installer/installation_files/nvim.sh
 
 # Fonts & getNF
-sudo apt install -y fonts-recommended \
-fonts-ubuntu fonts-font-awesome fonts-terminus
-fc-cache -f -v
+#sudo apt install -y fonts-recommended \
+#fonts-ubuntu fonts-font-awesome fonts-terminus
+#fc-cache -f -v
 
 # Pfetch
-wget https://github.com/dylanaraps/pfetch/archive/master.zip
-unzip master.zip
-sudo install pfetch-master/pfetch /usr/local/bin/
-ls -l /usr/local/bin/pfetch
-rm master.zip
+#wget https://github.com/dylanaraps/pfetch/archive/master.zip
+#unzip master.zip
+#sudo install pfetch-master/pfetch /usr/local/bin/
+#ls -l /usr/local/bin/pfetch
+#rm master.zip
 
 #Picom
 #Hitta korrekt version
@@ -49,30 +52,30 @@ rm master.zip
 #curl -sS https://starship.rs/install.sh | sh
 
 # Wallpapers
-cd && git clone https://github.com/abereg01/wallpapers.git
+#cd && git clone https://github.com/abereg01/wallpapers.git
 
 # Symlink
-git clone https://github.com/abereg01/dotfiles.git
-ln -s $HOME/dotfiles/.config/* $HOME/.config/
-ln -s $HOME/dotfiles/scripts/ $HOME/scripts
+#git clone https://github.com/abereg01/dotfiles.git
+#ln -s $HOME/dotfiles/.config/* $HOME/.config/
+#ln -s $HOME/dotfiles/scripts/ $HOME/scripts
 # Xsession & DWM
-if [[ ! -d /usr/share/xsessions ]]; then
-	sudo mkdir /usr/share/xsessions
-fi
-
-cat > ./temp << "EOF"
-[Desktop Entry]
-Encoding=UTF-8
-Name=dwm
-Comment=Dynamic window manager
-Exec=dwm
-Icon=dwm
-Type=Xsession
-EOF
-sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
-
-cd $HOME/dotfiles/.config/suckless/dwm/ && sudo make clean install
+#if [[ ! -d /usr/share/xsessions ]]; then
+#       sudo mkdir /usr/share/xsessions
+#fi
+#
+#cat > ./temp << "EOF"
+#[Desktop Entry]
+#Encoding=UTF-8
+#Name=dwm
+#Comment=Dynamic window manager
+#Exec=dwm
+#Icon=dwm
+#Type=Xsession
+#EOF
+#sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
+#
+#cd $HOME/dotfiles/.config/suckless/dwm/ && sudo make clean install
 
 #Installer Removal
-cd && rm -rf $HOME/installer/
+#cd && rm -rf $HOME/installer/
 echo 'Installation done. Reboot.'
